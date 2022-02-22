b94576fde0e485f55caa7e9d632129e5c248c0e8
Sean <seanchang@Seans-Mac-Mini.local>
2022-02-22 17:04:01 +0800

    Add logo

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -13,13 +13,32 @@
 // limitations under the License.
 import React from "react";
 import { Slide, Heading, FlexBox, Text } from "spectacle";
+import ParticleImage from "react-particle-image";
 import Background from '../assets/img/background.jpeg'
+import Logo from "../assets/img/logo.png"
+
+const particleOptions = {
+  filter: ({ x, y, image }) => {
+    // Get pixel
+    const pixel = image.get(x, y);
+    // Make a particle for this pixel if blue > 50 (range 0-255)
+    return pixel.r > 200;
+  },
+  color: ({ x, y, image }) => {
+    const pixel = image.get(x, y)
+    if (pixel.r > 200 & pixel.g < 50) {
+      return 'red'
+    }
+    return 'black'
+  }
+};
 
 const Cover = (props) => {
   return (
     <>
       <Slide backgroundImage={'url(' + Background + ')'} backgroundOpacity={0.8}>
         <FlexBox height="100%" flexDirection="column">
+          <ParticleImage src={Logo} scale={1.5} entropy={10} particleOptions={particleOptions} backgroundColor={'rgb(0, 0, 0, 0)'} height={200} />
           <Heading>TSMC Onboarding</Heading>
           <FlexBox flexDirection={"column"}>
             <Text margin='0.1em'>Sean Chang</Text>
